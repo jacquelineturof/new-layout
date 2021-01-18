02e0741dcdac1b83c2bc602b4224c3246a5140ef
jacquelineturof <jacquelineturof@gmail.com>
2021-01-17 23:05:48 -0500

    added main grid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css'
 
 import Layout from './components/Layout'
+import MainGrid from './components/MainGrid'
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        
+        <MainGrid />
       </Layout>
     </div>
   )
---
+++ src/components/MainGrid/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import classes from './MainGrid.module.css'
+
+const MainGrid = () =>  (
+    <div className = { classes.Grid }>
+
+    </div>
+)
+
+export default MainGrid
