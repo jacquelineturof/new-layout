f01508f5e22427fc69a906cba2ba22f1004d9766
jacquelineturof <jacquelineturof@gmail.com>
2021-01-18 22:45:27 -0500

    added components to main grid

--- src/components/MainGrid/index.js
+++ src/components/MainGrid/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
+import GithubActivity from './GithubActivity'
+import CurrentlyLearning from './CurrentlyLearning'
+import Calendar from './Calendar'
+import AboutMe from './AboutMe'
+
 import classes from './MainGrid.module.css'
 
 const MainGrid = () =>  (
     <div className = { classes.Grid }>
-
+        <GithubActivity />
+        <CurrentlyLearning />
+        <Calendar />
+        <AboutMe />
     </div>
 )
 
